c362d6308ee4e41a80ae70d941d4a831621fca4f
opitweb <lxnauticapt@gmail.com>
2026-01-21 22:56:00 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import Index from "./pages/Index";
 
-createRoot(document.getElementById("root")!).render(<App />);
+function App() {
+  return (
+    <Index />
+  );
+}
+
+export default App;
