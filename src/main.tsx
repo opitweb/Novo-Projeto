5954f6bf82c134092b5d21179245adc945648949
opitweb <lxnauticapt@gmail.com>
2026-01-22 21:34:04 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async' // Essencial para o SEO funcionar
 import App from './App'
-import './index.css' // Mantenha o index.css que tem o :root { --background: 100% }
+import './App.css' // Esta linha ativa o fundo preto e as cores
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>,
 )
