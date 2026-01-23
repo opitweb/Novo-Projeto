0a21b9b9a3e38f19e1e61962082f209b544fa592
opitweb <lxnauticapt@gmail.com>
2026-01-23 16:16:43 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css' // Mude de App.css para index.css se for aqui que o Tailwind está configurado
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
