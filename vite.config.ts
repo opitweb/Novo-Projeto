00286ab2be3e67fdc2cd4afae1b9277dd1ceb61f
opitweb <lxnauticapt@gmail.com>
2026-01-23 16:02:27 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc' // Adicionado o -swc aqui
 
 export default defineConfig({
   plugins: [react()],
-});
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+})
