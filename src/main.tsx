860f98b859d0cf063501ad8dd55588ea738ca2ab
opitweb <lxnauticapt@gmail.com>
2026-01-23 15:56:34 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './App.css' // Esta linha ativa o fundo preto e as cores
+import './index.css' // Mude de App.css para index.css se for aqui que o Tailwind está configurado
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
