c6c243c88b2afa6c64937a7420ee28842ba73fcd
opitweb <lxnauticapt@gmail.com>
2026-01-24 14:45:46 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,22 +5,23 @@ import App from './App.tsx'
 import SobrePage from './components/pages/sobre/SobrePage.tsx'
 import ServiciosPage from './components/pages/servicios/ServiciosPage.tsx'
 import ContactoPage from './components/pages/contacto/ContactoPage.tsx'
-import { Navbar } from './components/Navbar' // Certifique-se de que a Navbar está exportada
-import { Footer } from './components/Footer' // Se você criou um componente de Footer
+import { Navbar } from './components/components/Navbar' // Ajuste o caminho se necessário
+import { Footer } from './components/components/Footer' // Ajuste o caminho se necessário
 import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* Colocando aqui, eles aparecem em TODAS as páginas */}
       <Navbar /> 
       
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/sobre" element={<SobrePage />} />
-        <Route path="/servicios" element={<ServiciosPage />} />
-        <Route path="/contacto" element={<ContactoPage />} />
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/sobre" element={<SobrePage />} />
+          <Route path="/servicios" element={<ServiciosPage />} />
+          <Route path="/contacto" element={<ContactoPage />} />
+        </Routes>
+      </div>
 
       <Footer />
     </BrowserRouter>
