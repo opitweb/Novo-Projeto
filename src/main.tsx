dfc2d27e14daf1fcd29a94877f422e0e63ea1099
opitweb <lxnauticapt@gmail.com>
2026-01-24 14:05:40 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx' 
+import SobrePage from './components/pages/sobre/SobrePage.tsx'
+import ServiciosPage from './components/pages/servicios/ServiciosPage.tsx'
+import ContactoPage from './components/pages/contacto/ContactoPage.tsx'
 import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Rota da Página Inicial (Landing Page) */}
+        <Route path="/" element={<App />} />
+        
+        {/* Rotas das novas páginas que você criou */}
+        <Route path="/sobre" element={<SobrePage />} />
+        <Route path="/servicios" element={<ServiciosPage />} />
+        <Route path="/contacto" element={<ContactoPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
