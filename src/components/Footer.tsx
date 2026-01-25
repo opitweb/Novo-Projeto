7af8579f035da02de6705340ac0bc7ec880e5a95
opitweb <lxnauticapt@gmail.com>
2026-01-25 18:30:40 +0100

    Replace useLayoutEffect with useEffect in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React, {  useLayoutEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
