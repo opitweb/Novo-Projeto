87e56cb2d553012060a3566934bc80de41ed8f8b
opitweb <lxnauticapt@gmail.com>
2026-01-25 13:01:24 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,5 +39,12 @@ export const Footer = () => {
           <p className="text-blue-100/30 text-xs tracking-widest uppercase font-medium">
             Marketing Médico de Alto Nivel
           </p>
+          {/* Linha de Copyright */}
           <p className="pt-10 border-t border-white/5 w-full text-[9px] uppercase tracking-[0.3em] font-bold text-white/40">
-            © Betterfly Media 20
+            © Betterfly Media 2026 — Barcelona & Santiago
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
