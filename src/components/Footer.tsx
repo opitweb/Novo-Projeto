607d2a90b4726ae391ad9144c83ac43e82a035df
opitweb <lxnauticapt@gmail.com>
2026-01-25 18:20:49 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React, {  useLayoutEffect, useRef } from "react";
+import React, {  "use client", useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
