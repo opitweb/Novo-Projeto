5068dd0e41423141b88eedd8c99ab3262e3a7a51
opitweb <lxnauticapt@gmail.com>
2026-01-26 16:17:30 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 // IMPORTAÇÃO DO CHATBOT
-import Chatbot from './components/Chatbot/Chatbot';
+import Chatbot from './components/chatbot/Chatbot';
 
 export default function Index() {
   const [offset, setOffset] = useState(0);
